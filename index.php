529c8c897a90b7204713a9a16651bf3faaddbced
kreostigma <kreostigma@users.noreply.github.com>
2020-08-14 17:25:18 +0300

    Update index.php

--- index.php
+++ index.php
@@ -96,9 +96,9 @@ $products = [
         <p class="promo__text">На нашем интернет-аукционе ты найдёшь самое эксклюзивное сноубордическое и горнолыжное снаряжение.</p>
         <ul class="promo__list">
             <!--заполните этот список из массива категорий-->
-        <?php foreach ($categories as $key => $value): ?>
+        <?php foreach ($categories as $category): ?>
          <li class="promo__item promo__item--boards">
-            <a class="promo__link" href="pages/all-lots.html"><?=$categories[$key];?></a>
+            <a class="promo__link" href="pages/all-lots.html"><?=$category;?></a>
          </li>
         <?php endforeach ?>
         </ul>
@@ -108,6 +108,22 @@ $products = [
         <div class="lots__header">
             <h2>Открытые лоты</h2>
         </div>
+    <?php
+        $r = rand (0.0, 10000.99);
+        function form_price($r)
+            {
+                if ($r < 1000)
+                    {
+                    echo ceil($r) . " ₽";
+                    }   
+                else
+                    {
+                    $f = ceil($r);
+                    echo number_format($f, 2, '.', ' ') . " ₽";
+                    } 
+
+            }
+    ?>
         <ul class="lots__list">
             <!--заполните этот список из массива с товарами-->
             <?php foreach ($products as $product): ?>
@@ -120,8 +136,8 @@ $products = [
                     <h3 class="lot__title"><a class="text-link" href="pages/lot.html"><?= $product['name']; ?></a></h3>
                     <div class="lot__state">
                         <div class="lot__rate">
-                            <span class="lot__amount"><?= $product['price']; ?></span>
-                            <span class="lot__cost"><?= $product['price']; ?><b class="rub">р</b></span>
+                            <span class="lot__amount"><?= form_price($product['price']); ?></span>
+                            <span class="lot__cost"><?= form_price($product['price']); ?></span>
                         </div>
                         <?php endforeach ?>
                         <div class="lot__timer timer">
